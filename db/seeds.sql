46d8447907dc258cc940029d8a759232783da5ae
Kaylee Horner <kayleeahorner@gmail.com>
2020-12-08 23:53:02 -0600

    initial add with schema and seeds written out

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Hamburger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Cheeseburger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheeseburger', false);
+
