47ded1a76b64480d84df3d4722903a602f74ea28
Kaylee Horner <kayleeahorner@gmail.com>
2020-12-14 00:10:08 -0600

    adding completed server file

--- server.js
+++ server.js
@@ -1,1 +1,20 @@
 const express = require("express");
+const PORT = process.env.PORT || 8080;
+const app = express();
+const handlebars = require("express-handlebars");
+const routes = require("./controllers/burger_controller.js");
+
+
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(routes);
+
+app.engine("handlebars", handlebars({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+
+// Start server
+app.listen(PORT, function() {
+  console.log("Server listening on: http://localhost:" + PORT);
+});
